f28dad066283c0204922ddbaedb11e996d83e0e1
Tianze Shou <shoutianze@outlook.com>
2023-02-25 12:22:42 -0500

    fix TA account type validation

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -105,7 +105,9 @@ authenticationController.register = async function (req, res) {
         }
 
         if (!userData['account-type'] ||
-            (userData['account-type'] !== 'student' && userData['account-type'] !== 'instructor')) {
+            (userData['account-type'] !== 'student' && 
+             userData['account-type'] !== 'instructor' && 
+             userData['account-type'] !== 'TA')) {
             throw new Error('Invalid account type');
         }
 
