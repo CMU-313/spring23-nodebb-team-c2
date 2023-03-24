04d322ae6c2dd5264547b321de6516c9f941746d
Victor Huang <victorhuangwq@gmail.com>
2023-03-24 16:52:57 -0400

    update todo

--- src/controllers/write/career.js
+++ src/controllers/write/career.js
@@ -20,7 +20,7 @@ Career.register = async (req, res) => {
             num_past_internships: userData.num_past_internships,
         };
 
-        userCareerData.prediction = Math.round(Math.random()); // TODO: Somehow call model ???
+        userCareerData.prediction = Math.round(Math.random()); // TODO: Change this line to do call and retrieve actual candidate success prediction from the model instead of using a random number
 
         await user.setCareerData(req.uid, userCareerData);
         db.sortedSetAdd('users:career', req.uid, req.uid);
