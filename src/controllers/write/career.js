da6d0e67333bf0531554479a330c312bb32c261c
Doreen Valmyr <dvalmyr@andrew.cmu.edu>
2023-04-09 02:10:22 -0400

    make post request to flask service to obtain career prediction

--- src/controllers/write/career.js
+++ src/controllers/write/career.js
@@ -6,6 +6,43 @@ const db = require('../../database');
 
 const Career = module.exports;
 
+function fetchPrediction(userData) {
+    return new Promise((resolve, reject) => {
+        fetch('https://ml-career.fly.dev/prediction', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                student_id: userData.student_id,
+                major: userData.major,
+                age: userData.age,
+                gender: userData.gender,
+                gpa: userData.gpa,
+                extra_curricular: userData.extra_curricular,
+                num_programming_languages: userData.num_programming_languages,
+                num_past_internships: userData.num_past_internships
+            })
+        })
+        .then(response => {
+            if (!response.ok) {
+              throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+          })
+        .then(data => {
+            resolve(data.good_employee);
+            console.log(data);
+            // Access the response parameters here
+            console.log(data.good_employee);
+        })
+        .catch(error => {
+            reject(error);
+            console.error('Error:', error);
+        });
+    });
+}
+
 Career.register = async (req, res) => {
     const userData = req.body;
     try {
@@ -20,9 +57,7 @@ Career.register = async (req, res) => {
             num_past_internships: userData.num_past_internships,
         };
 
-        // TODO: Change this line to do call and retrieve actual candidate success
-        // prediction from the model instead of using a random number
-        userCareerData.prediction = Math.round(Math.random());
+        userCareerData.prediction = await fetchPrediction(userData);;
 
         await user.setCareerData(req.uid, userCareerData);
         db.sortedSetAdd('users:career', req.uid, req.uid);
